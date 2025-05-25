b775515cad511980a6401226d24323a605d19895
Rodro <rodro@Rodros-MacBook-Air.local>
2025-05-25 17:35:46 +0600

    student model

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,90 @@
+import { Schema, model, connect } from 'mongoose';
+import { Guardian, Student, Student, localGuardian, userName } from './student/student.interface';
+
+const userNameSchema = new Schema<userName>({
+  
+    fistName: {
+      type: String,
+      required: true
+    },
+    middleName:{
+      type: String,
+    },
+    lastName:{
+      type: String,
+      required: true
+    }
+  
+})
+
+const guardianSchema = new Schema <Guardian>(
+  {
+    fatherName:{
+      type: String
+    },
+  fatherOccupation: {
+    type: String
+  },
+  fatherContact: {
+    type: String
+  },
+  motherName: {
+    type: String
+  },
+  motherOccupation: {
+    type: String
+  },
+  motherContact: {
+    type: String
+  },
+  }
+)
+
+const localGuardianSchema = new Schema <localGuardian> (
+  {
+    name: {
+      type: String
+    },
+  occupation :{
+    type: String
+  },
+  contact: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  }
+)
+
+const studentSchema = new Schema < Student> ({
+  id: {type: String},
+  name:userNameSchema,
+  gender:['male','female'],
+  dateOfBirth: {type: String},
+  email:{
+    type : String,
+    required: true
+  },
+  contactNumber: {
+    type: String,
+    required: true
+  },
+  BloodGroup: ["A+", "B+", "A-"],
+  presentAddress:{
+    type : String,
+    required: true
+  },
+  permanentAddress:{
+    type : String,
+    required: true
+  },
+  Guardian:guardianSchema,
+  localGuardian:localGuardianSchema,
+  profileImg: {type: String},
+  isActive:['Active','Inactive']
+})
+
+
+// creating model
+const Student = model<Student>('Student',studentSchema)
--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,16 +1,41 @@
 import { Schema, model, connect } from 'mongoose';
 
-export type student = {
+
+export type Guardian = {
+  fatherName: string,
+  fatherOccupation: string,
+  fatherContact: string,
+  motherName: string,
+  motherOccupation: string,
+  motherContact: string,
+}
+
+export type userName = {
+  fistName: string;
+  middleName: string;
+  lastName: string;
+}
+
+export type localGuardian = {
+  name: string;
+  occupation :string;
+  contact: string;
+  address: string;
+}
+
+export type Student = {
   id: string;
-  name: {
-    fistName: string;
-    middleName: string;
-    lastName: string;
-  },
+  name: userName;
   dateOfBirth: string;
   contactNumber: string;
   gender: 'male' | 'female';
   email: string;
   emergencyContact: string;
-  BloodGroup: "A+"| "B+"| "A-"
+  BloodGroup: "A+"| "B+"| "A-";
+  presentAddress: string;
+  permanentAddress: string;
+  Guardian:  Guardian;
+  localGuardian: localGuardian;
+  profileImg ?: string;
+  isActive: 'Active' | 'Inactive';
 }
