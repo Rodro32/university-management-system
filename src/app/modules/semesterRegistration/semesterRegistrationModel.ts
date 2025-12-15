cb7471dc3dd4816cd37076a540711798162abd80
Rodro <rodro@Rodros-MacBook-Air.local>
2025-12-15 22:12:07 +0600

    semester registration

---
+++ src/app/modules/semesterRegistration/semesterRegistrationModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import { TSemesterRegistration } from "./semesterRegistrationInterface";
+import { semesterRegistrationStatus } from "./semesterRegistrationConstant";
+
+const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>({
+  academicSemester:{
+    type:Schema.Types.ObjectId,
+    unique:true,
+    required:true,
+    ref:'AcademicSemester'
+  },
+  status:{
+    type:String,
+    enum:semesterRegistrationStatus,
+    default:'UPCOMING',
+  },
+  startDate:{
+    type:Date,
+    required:true,
+  },
+  endDate:{
+    type:Date,
+    required:true,
+  },
+  minCredit:{
+    type:Number,
+    default:3
+  },
+  maxCredit:{
+    type:Number,
+    default:14
+  }
+},{
+  timestamps:true
+})
+
+export const SemesterRegistration = mongoose
+.model<TSemesterRegistration>('SemesterRegistration',semesterRegistrationSchema)
